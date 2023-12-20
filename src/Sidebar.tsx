9bfa47411ae01b565197630d04a94ee1202df72a
RNA <rna0@protonmail.com>
2023-12-20 16:59:15 +0200

    fixed any type using and add new redirection when no login found

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,16 +1,12 @@
 import {NavLink} from 'react-router-dom';
 import HorizonHubSvg from './assets/HorizonHub.svg';
-import { useAuthentication, useAuthenticationDispatch } from './store/hook/useAuthentication';
-import { useEffect } from 'react';
-import { useUserData } from './store/hook/useUserData';
+import {useAuthentication, useAuthenticationDispatch} from './store/hook/useAuthentication';
+import {useEffect} from 'react';
 
 interface INavLinkOption {
     name: string,
     link: string
 }
-  
-
-
 
 const NavLinkOption = (props: INavLinkOption) =>
     <li className='px-4 py-3'>
