2b87ec29d7d8a788324b000b7155e1b29538224d
MrGlam <elonglam@gmail.com>
2024-01-08 21:26:31 +0200

    extract user id from token

---
+++ src/utillity/jwt.tsx
@@ -0,0 +1,18 @@
+import { jwtDecode } from 'jwt-decode' // import dependency
+
+interface DecodedToken {
+  _id: string;
+  // Add other fields from your JWT payload
+}
+
+export const extractUserIdFromToken = (token: string): string | null => {
+  try {
+    const decoded: DecodedToken = jwtDecode(token);
+    console.log(decoded);
+    
+    return decoded._id;
+  } catch (error) {
+    console.error('Error decoding JWT token:', error);
+    return null;
+  }
+};
